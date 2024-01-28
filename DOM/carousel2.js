9776bab98244e2ae7fd6baf2943ec0cbb726c8ac
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2024-01-28 20:58:47 +0530

    carousel next and prev

--- DOM/carousel2.js
+++ DOM/carousel2.js
@@ -1,15 +1,51 @@
-let mL = 100;
+// let mL = 100;
 
-setInterval(() => {
+// setInterval(() => {
+//   document.getElementById('carousel').style.transition = '1s';
+//   document.getElementById('carousel').style.marginLeft = -mL + '%';
+//   mL += 100;
+
+//   if (mL > 400) {
+//     setTimeout(() => {
+//       document.getElementById('carousel').style.transition = '0s';
+//       document.getElementById('carousel').style.marginLeft = '0%';
+//       mL = 100;
+//     }, 1000);
+//   }
+// }, 3000);
+
+let currentSlide = 1;
+
+goToSlide = (slideNumber) => {
   document.getElementById('carousel').style.transition = '1s';
-  document.getElementById('carousel').style.marginLeft = -mL + '%';
-  mL += 100;
+  document.getElementById('carousel').style.marginLeft =
+    -slideNumber * 100 + '%';
+};
 
-  if (mL > 400) {
+document.getElementById('next').addEventListener('click', () => {
+  currentSlide++;
+  console.log(currentSlide);
+  goToSlide(currentSlide);
+
+  if (currentSlide > 4) {
     setTimeout(() => {
       document.getElementById('carousel').style.transition = '0s';
-      document.getElementById('carousel').style.marginLeft = '0%';
-      mL = 100;
+      document.getElementById('carousel').style.marginLeft = '-100%';
+      currentSlide = 1;
     }, 1000);
   }
-}, 3000);
+});
+
+document.getElementById('previous').addEventListener('click', () => {
+  currentSlide--;
+  console.log(currentSlide);
+  goToSlide(currentSlide);
+
+  if (currentSlide === 0) {
+    setTimeout(() => {
+      document.getElementById('carousel').style.transition = '0s';
+      document.getElementById('carousel').style.marginLeft = '-400%';
+      currentSlide = 4;
+    }, 1000);
+  }
+});
